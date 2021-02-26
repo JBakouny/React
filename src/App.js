1de572ea4e3180fd1719778c9e623deeff45b748
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-02-26 10:29:14 +0200

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { Component } from 'react';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }

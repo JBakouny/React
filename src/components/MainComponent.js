a3edfd004a6bc7e20b83c1249d31a2d748330859
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-03-08 23:23:17 +0200

    Remove useless line in MainComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';

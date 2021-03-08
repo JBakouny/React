52b09a92185d2310e889dd3ac16e5a7ed283747f
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-03-09 00:49:56 +0200

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent'
+import About from './AboutComponent'
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -50,6 +51,7 @@ class Main extends Component {
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
           <Redirect to="/home" />
         </Switch>
         <Footer/>     

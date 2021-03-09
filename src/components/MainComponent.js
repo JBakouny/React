81ab062fb55d633216b5ea662bcb2051d66bc7fb
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-03-09 10:01:29 +0200

    Introduce AboutPage anonymous function

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -42,6 +42,8 @@ class Main extends Component {
       );
     };
 
+    const AboutPage = () => <About leaders={this.state.leaders}/>
+
 
     return (
       <div>
@@ -51,7 +53,7 @@ class Main extends Component {
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
-          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
+          <Route exact path='/aboutus' component={AboutPage} />
           <Redirect to="/home" />
         </Switch>
         <Footer/>     

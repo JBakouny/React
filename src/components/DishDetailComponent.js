e0e7696a083b6f86e784db817f5f04296351fa4d
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-03-16 00:01:17 +0200

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,11 +5,12 @@ import { Card, CardImg, CardBody,CardText, Button, Modal, ModalHeader, ModalBody
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDish({dish}) {
     return (
       <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>

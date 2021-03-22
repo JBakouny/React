15beb5bc6804b526ff5915f77cca22481737b896
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-03-22 16:42:49 +0200

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,33 +6,40 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderDish({dish}) {
     return (
-      <Card>
-        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     )
   }
 
   function RenderComments({comments, postComment, dishId}) {
-    const comnts = comments.map(comment => {
-        return (
-            <li key={comment.id}>
+    const comnts = <Stagger in>
+        {comments.map((comment) => {
+            return (
+                <Fade in>
+                <li key={comment.id}>
                 <p>{comment.comment}</p>
-                <p>-- {comment.author},
-                &nbsp;
-                {new Intl.DateTimeFormat('en-US', 
-                { year: 'numeric', month: 'short', day: '2-digit'})
-                .format(new Date(Date.parse(comment.date)))}
-                </p>
-            </li>
-        )
-    });
+                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                </li>
+                </Fade>
+            );
+        })}
+    </Stagger>;
+
     if (comments == null) {
         return <div></div>
     }
